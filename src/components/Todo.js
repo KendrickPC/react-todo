4789f8de985a8a81da60f1ac91303693024f298c
Kenneth P Chang <kennethpchang@gmail.com>
2021-06-28 13:02:19 -0700

    Button for completed task working

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -3,16 +3,25 @@ import React from 'react';
 export const Todo = ({text, todo, todos, setTodos}) => {
   // Event handler for deleting items
   const deleteHandler = () => {
-    setTodos(todos.filter((el) => (el.id !== todo.id)))
+    setTodos(todos.filter(el => el.id !== todo.id))
     console.log(todo);
+  }
+
+  const completeHandler = () => {
+    setTodos(todos.map(item => {
+      if (item.id === todo.id) {
+        return {...item, completed: !item.completed}
+      }
+    }))
   }
 
   return (
     <div className="todo">
-      <li className="todo-item">{text}</li>
-      <button className="complete-btn">
+      <li className={`todo-item ${todo.completed ? "completed" : ""}`}>{text}</li>
+      <button onClick={completeHandler} className="complete-btn">
         <i className="fas fa-check"></i>
       </button>
+
       <button onClick={deleteHandler} className="trash-btn">
         <i className="fas fa-trash"></i>
       </button>
